8e7b0d97fe46e96edd17487f138ea31eac2c2934
Chrischale Panditharathne <cpanda27@bu.edu>
2020-08-03 16:57:56 -0400

    added User Display Components views for Admin/User

--- src/components/User-Display-Components/UserCardDisplayComponent.tsx
+++ src/components/User-Display-Components/UserCardDisplayComponent.tsx
@@ -1,62 +1,96 @@
-import React, { FunctionComponent, useEffect, useState } from 'react'
-import { makeStyles } from '@material-ui/core/styles';
-import { Card, CardActionArea, CardMedia, CardContent, Typography, CardActions, Button } from '@material-ui/core'
-import { Link } from 'react-router-dom';
+//display a user's profile in partial detail
 
+import 'react-toastify/dist/ReactToastify.css';
+import React, { FunctionComponent, useState, useEffect, SyntheticEvent } from 'react'
+import { User } from '../../models/User'
+import { useParams } from 'react-router'
+import { getUserProfile } from '../../remote/user-service/getUserProfile'
+import { Grid, makeStyles, CardActions, CardActionArea, Card, CardContent, Typography, CardMedia, Button } from '@material-ui/core'
+import { TitleComponent } from '../Title-Component/TitleComponent';
+import { Link } from 'react-router-dom'
 
-export const DisplayUserCardComponent:FunctionComponent<any> = (props) => {
-    //get all the user infomation 
-    
-    let classes = useStyles()
-    //change into a card later if you have time ... 
-    return(
-        <div className = {classes.root}
-        style={{
-            display: "flex",
-            justifyContent: "center",
-            alignItems: "center"
-          }}>
-              
-        <Card>
-        <CardActionArea>
-            <CardMedia
-                className={classes.media}
-                image=""
-                //the user image would go here
-                title="User Profile Picture"
-            />
-            <CardContent>
-            <Typography gutterBottom variant="h5" component="h2">
-                Name: {props.user?.firstName} {props.user?.lastName}
-            </Typography>
-            <Typography variant="body2" color="textSecondary" component="p">
-                Affiliation: {props.user?.affiliation}
-            </Typography>
-                    
-            </CardContent>
-            </CardActionArea>
-            <CardActions>
-                <Button variant="outlined" color="primary">
-                <Link to={`/profile/${props.user.userId}`}>
-                    Details
-                </Link>
-                </Button>
-                
-                
-        </CardActions>
-        </Card> 
-        <br/>          
-    
-        </div>
-    )
-    
-}
 
 const useStyles = makeStyles({
     root: {
         maxWidth: 345,
-    },
-    media: {
+      },
+      media: {
         height: 140,
+      },
+    card: {
+      display: 'flex',
     },
-});
+    cardDetails: {
+      flex: 1,
+    },
+    cardMedia: {
+      width: 160,
+    },
+  });
+
+
+export const UserCardDisplayComponent   :FunctionComponent<any> = (props) => {
+    let[userProfile, changeUserProfile] = useState<User|null>(null)
+    let {userId} = useParams()
+
+    useEffect(()=>{
+        //we define an async operation we want to run
+        let getUser = async ()=>{
+            //we await user info and then call a state updat function with it
+            let userInfo = await getUserProfile(userId)
+            changeUserProfile(userInfo)
+        }
+        //if we haven't gotten a user profile yet
+        if(!userProfile || userProfile.userId !== +userId){
+            //go get the user
+            getUser()
+        }
+        //else do nothing
+    })
+   
+    const classes = useStyles(); 
+ 
+    return(
+        
+        (userProfile)?
+        <Card className={classes.root}>
+            <CardActionArea>
+               <CardMedia
+                className={classes.media}
+                image={userProfile.image}
+               />
+                <CardContent>
+                    {/* Name of User: */}
+                <Typography gutterBottom variant="h5" component="h2">
+                    {userProfile.firstName} {userProfile.lastName}
+                </Typography>
+                <Typography variant="body2" color="textSecondary" component="p">
+                    AFFILIATION: {userProfile.affiliation}
+                </Typography>
+                <Typography variant="body2" color="textSecondary" component="p">
+                    ROLE: {userProfile.role}
+                </Typography>
+                <Typography variant="body2" color="textSecondary" component="p">
+                    PLACES VISITED: {userProfile.placesVisited}
+                </Typography>
+
+
+                </CardContent>
+            </CardActionArea>
+            <CardActions>
+                <Button size="small" color="primary">
+                Contact
+                </Button>
+                <Button size="small" color="primary">
+                Learn More
+                </Button>
+            </CardActions>
+        </Card>
+        
+        :
+        <div>
+            <h3> User Not Found</h3>
+        </div>
+    )
+
+}
